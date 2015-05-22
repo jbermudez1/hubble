e6c890373d5142d21385b1d5c11398fee3959180
Javier B <javierbermudez@MacBook-Pro-Retina.local>
2015-05-22 12:31:49 -0600

    Actualizacion

--- js/hubble.js
+++ js/hubble.js
@@ -3,6 +3,25 @@ $( document ).ready( inicio );
 function inicio () {
 	dibujasvg();
 	// body...
+	$( ".topMenus" ).load( "menus.html", function(){
+			$( ".showMenu" ).click(function() {
+		  		$(".menuLateral").toggleClass("oculto");
+		  		$("html").toggleClass("disableScroll");
+
+			});
+	} );
+
+
+
+	$(".conoceMas").click( function(){
+		window.location = "detalles.html" ;
+	});
+	$(".irComenzar").click( function(){
+		window.location = "http://app.hubble.mx/empresa" ;
+	});
+
+	$("footer").load("footer.html");
+
 }
 
 function dibujasvg(){
